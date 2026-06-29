06fc21c2385b53073ba81a8ad03794f3ff6eae35
prabin pal <111887132+2006-prabinpal@users.noreply.github.com>
2026-06-29 08:23:25 +0545

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,21 @@
+// Function Declaration
+function sum(a, b) {
+    return a + b;
+}
 
+console.log(sum(2, 3)); 
+
+
+// Function Expression
+const fun2 = function (a, b) {
+    return a + b;
+};
+
+console.log(fun2(4, 6)); 
+
+
+
+(function () {
+    console.log("life", 6 + 7);
+    return 6 + 7;
+})();
