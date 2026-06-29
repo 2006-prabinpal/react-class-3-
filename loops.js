de727ad4af5a4f108a00f7da208033720196e318
prabin pal <111887132+2006-prabinpal@users.noreply.github.com>
2026-06-29 08:20:26 +0545

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,1 +1,35 @@
+FOR LOOPS 
+
+
+for (let i = 0; i <10; i++) {
+    if ([]) {
+         console.log(i);
+     }
+         
+ }
+
+ //falsy values
+// 0 False undefined null Nan "":
+// 
+// Declare and initialize the variable
+
+
+WHILE LOOPS 
+
+
+let i = 0;
+
+// The do block runs at least once
+do {
+
+    // Check if i is even
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+
+    // Increase i by 1
+    i++;
+
+} while (i < 10);
+
 
